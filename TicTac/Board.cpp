3ae8c9a07f002a534919d0e34c394603c457a7ac
DalenWBrauner <DalenWBrauner@gmail.com>
2014-04-24 16:21:46 -0400

    Added checking for which and whose turn it is and made HOW_EMPTY accurate

--- TicTac/Board.cpp
+++ TicTac/Board.cpp
@@ -19,8 +19,8 @@ class ToeBoard
 		ToeBoard(boolGrid xoxo, boolGrid empty);			// Create preset board
 		ToeBoard(bool dbg, boolGrid xoxo, boolGrid empty);	// Create preset board w/ debug setting
 
-	// Allows output via '<<'
 	friend ostream& operator<<(ostream& s, ToeBoard tb)
+	/* Allows output via '<<' */
 	{
 		for (auto x=0; x<3; x++) {
 			for (auto y=0; y<3; y++) {
@@ -33,8 +33,9 @@ class ToeBoard
 		s << "\n";
 		return(s);
 	}
-	/* Check private variables */
+
 	void checkYourself()
+	/* Couts private variables */
 	{
 		// debug
 		cout << "debug: " << debug << ";\n";
@@ -64,6 +65,31 @@ class ToeBoard
 		// HOW_EMPTY
 		cout << "HOW_EMPTY: " << HOW_EMPTY << ";\n";
 	}
+
+	short whichTurn()
+	/* Returns how many turns have passed */
+	{ return (9 - HOW_EMPTY); }
+	
+	bool whoseTurn()
+	/* Returns 1 if X's turn, 0 if O's turn */
+	{
+		short Xs = 0;
+		short Os = 0;
+		for (auto x=0; x<3; x++) {
+			for (auto y=0; y<3; y++) {
+				if (!EMPTY[x][y])
+				{
+					if (XOXO[x][y])	{Xs++;}
+					else			{Os++;}
+				}
+			}
+		}
+		// X always goes first
+		if (Xs > Os)	{return(0);}
+		else			{return(1);}
+	}
+	
+			
 };
 
 /* Create empty board */
@@ -85,7 +111,7 @@ ToeBoard::ToeBoard(bool dbg)
 	HOW_EMPTY	= 9;
 	if (dbg)
 	{
-		cout << "Empty board w/ debug?\n";
+		cout << "Creating Empty board w/ debug...\n";
 		checkYourself();
 	}
 }
@@ -96,7 +122,12 @@ ToeBoard::ToeBoard(boolGrid xoxo, boolGrid empty)
 	debug		= 0;
 	XOXO		= xoxo;
 	EMPTY		= empty;
-	HOW_EMPTY	= 9;
+	HOW_EMPTY	= 0;
+	for (auto x=0; x<3; x++) {
+		for (auto y=0; y<3; y++) {
+			if(EMPTY[x][y]) {HOW_EMPTY++;}
+		}
+	}
 }
 
 
@@ -106,10 +137,16 @@ ToeBoard::ToeBoard(bool dbg, boolGrid xoxo, boolGrid empty)
 	debug		= dbg;
 	XOXO		= xoxo;
 	EMPTY		= empty;
-	HOW_EMPTY	= 9;
+	HOW_EMPTY	= 0;
+	for (auto x=0; x<3; x++) {
+		for (auto y=0; y<3; y++) {
+			if(EMPTY[x][y]) {HOW_EMPTY++;}
+		}
+	}
+
 	if (dbg)
 	{
-		cout << "Preset board w/ debug?\n";
+		cout << "Creating board w/ debug...\n";
 		checkYourself();
 	}
 }
--- TicTac/Test_Board.cpp
+++ TicTac/Test_Board.cpp
@@ -4,40 +4,94 @@ using namespace std;
 
 void Test01()
 {
-	// Outputs an entire game of TicTacToe
+	// Outputs what appears to be an entire game of TicTacToe
 	ToeBoard t0;
-	cout << "\nTurn 0:\n" << t0;
-
 	ToeBoard t1( {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{1,1,1},{1,1,1}} );
-	cout << "\nTurn 1:\n" << t1;
-
 	ToeBoard t2( {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{1,1,0},{1,1,1}} );
-	cout << "\nTurn 2:\n" << t2;
-
 	ToeBoard t3( {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{1,1,0},{0,1,1}} );
-	cout << "\nTurn 3:\n" << t3;
-
 	ToeBoard t4( {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{1,0,0},{0,1,1}} );
-	cout << "\nTurn 4:\n" << t4;
-
 	ToeBoard t5( {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{0,0,0},{0,1,1}} );
-	cout << "\nTurn 5:\n" << t5;
-
 	ToeBoard t6( {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{0,0,0},{0,0,1}} );
-	cout << "\nTurn 6:\n" << t6;
-
 	ToeBoard t7( {{0,1,1},{1,0,0},{1,0,1}}, {{1,0,0},{0,0,0},{0,0,1}} );
-	cout << "\nTurn 7:\n" << t7;
-
-	ToeBoard t8( {{0,1,1},{1,0,0},{1,0,1}}, {{1,0,0},{0,0,0},{0,0,0}} );
-	cout << "\nTurn 8:\n" << t8;
-
+	ToeBoard t8( {{0,1,1},{1,0,0},{1,0,1}}, {{0,0,0},{0,0,0},{0,0,1}} );
 	ToeBoard t9( {{0,1,1},{1,0,0},{1,0,1}}, {{0,0,0},{0,0,0},{0,0,0}} );
+	cout << "\nTurn 0:\n" << t0;
+	cout << "\nTurn 1:\n" << t1;
+	cout << "\nTurn 2:\n" << t2;
+	cout << "\nTurn 3:\n" << t3;
+	cout << "\nTurn 4:\n" << t4;
+	cout << "\nTurn 5:\n" << t5;
+	cout << "\nTurn 6:\n" << t6;
+	cout << "\nTurn 7:\n" << t7;
+	cout << "\nTurn 8:\n" << t8;
 	cout << "\nTurn 9:\n" << t9;
+}
+
+void Test02()
+{
+	// Runs debug on an entire game of TicTacToe
+	ToeBoard t0(1);
+	ToeBoard t1(1, {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{1,1,1},{1,1,1}} );
+	ToeBoard t2(1, {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{1,1,0},{1,1,1}} );
+	ToeBoard t3(1, {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{1,1,0},{0,1,1}} );
+	ToeBoard t4(1, {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{1,0,0},{0,1,1}} );
+	ToeBoard t5(1, {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{0,0,0},{0,1,1}} );
+	ToeBoard t6(1, {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{0,0,0},{0,0,1}} );
+	ToeBoard t7(1, {{0,1,1},{1,0,0},{1,0,1}}, {{1,0,0},{0,0,0},{0,0,1}} );
+	ToeBoard t8(1, {{0,1,1},{1,0,0},{1,0,1}}, {{0,0,0},{0,0,0},{0,0,1}} );
+	ToeBoard t9(1, {{0,1,1},{1,0,0},{1,0,1}}, {{0,0,0},{0,0,0},{0,0,0}} );
+}
+
+void Test03()
+{
+	// Checks whose turn it is throughout the game
+	ToeBoard t0;
+	ToeBoard t1( {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{1,1,1},{1,1,1}} );
+	ToeBoard t2( {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{1,1,0},{1,1,1}} );
+	ToeBoard t3( {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{1,1,0},{0,1,1}} );
+	ToeBoard t4( {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{1,0,0},{0,1,1}} );
+	ToeBoard t5( {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{0,0,0},{0,1,1}} );
+	ToeBoard t6( {{0,1,1},{1,0,0},{1,0,1}}, {{1,1,0},{0,0,0},{0,0,1}} );
+	ToeBoard t7( {{0,1,1},{1,0,0},{1,0,1}}, {{1,0,0},{0,0,0},{0,0,1}} );
+	ToeBoard t8( {{0,1,1},{1,0,0},{1,0,1}}, {{0,0,0},{0,0,0},{0,0,1}} );
+	ToeBoard t9( {{0,1,1},{1,0,0},{1,0,1}}, {{0,0,0},{0,0,0},{0,0,0}} );
+
+	cout << "On turn " << t0.whichTurn();
+	if (t0.whoseTurn())	{ cout << ", it's X's turn!\n"; }
+	else				{ cout << ", it's O's turn!\n"; }
+	cout << "On turn " << t1.whichTurn();
+	if (t1.whoseTurn())	{ cout << ", it's X's turn!\n"; }
+	else				{ cout << ", it's O's turn!\n"; }
+	cout << "On turn " << t2.whichTurn();
+	if (t2.whoseTurn())	{ cout << ", it's X's turn!\n"; }
+	else				{ cout << ", it's O's turn!\n"; }
+	cout << "On turn " << t3.whichTurn();
+	if (t3.whoseTurn())	{ cout << ", it's X's turn!\n"; }
+	else				{ cout << ", it's O's turn!\n"; }
+	cout << "On turn " << t4.whichTurn();
+	if (t4.whoseTurn())	{ cout << ", it's X's turn!\n"; }
+	else				{ cout << ", it's O's turn!\n"; }
+	cout << "On turn " << t5.whichTurn();
+	if (t5.whoseTurn())	{ cout << ", it's X's turn!\n"; }
+	else				{ cout << ", it's O's turn!\n"; }
+	cout << "On turn " << t6.whichTurn();
+	if (t6.whoseTurn())	{ cout << ", it's X's turn!\n"; }
+	else				{ cout << ", it's O's turn!\n"; }
+	cout << "On turn " << t7.whichTurn();
+	if (t7.whoseTurn())	{ cout << ", it's X's turn!\n"; }
+	else				{ cout << ", it's O's turn!\n"; }
+	cout << "On turn " << t8.whichTurn();
+	if (t8.whoseTurn())	{ cout << ", it's X's turn!\n"; }
+	else				{ cout << ", it's O's turn!\n"; }
+	cout << "On turn " << t9.whichTurn();
+	if (t9.whoseTurn())	{ cout << ", it's X's turn!\n"; }
+	else				{ cout << ", it's O's turn!\n"; }
 
 }
 
 int main()
 {
-	cout << "TEST01()\n";	Test01();
+	cout << "~~~~~\tTEST01()\n";	Test01();
+	cout << "~~~~~\tTEST02()\n";	Test02();
+	cout << "~~~~~\tTEST03()\n";	Test03();
 }
