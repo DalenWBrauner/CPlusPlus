d656012a5462d2682210216987c40998bde39bc9
DalenWBrauner <DalenWBrauner@gmail.com>
2014-05-07 17:11:38 -0400

    Gave the AI a stronger grasp on who it really is

--- TicTac/AI.cpp
+++ TicTac/AI.cpp
@@ -16,25 +16,92 @@ class AI
 		return(s);
 	}
 
+	short tryTurn(ToeBoard ttt, short x, short y)
+	// Returns the result of a suggested move
+	// Returns 7 if the move is impossible
+	{
+		if (ttt.gameOver() != -1)	{return 7;}
+		else if (!ttt.isEmpty(x,y)) {return 7;}
+		else
+		{
+			ttt.takeTurn(x,y,ttt.whoseTurn());
+			return ttt.gameOver();
+		}
+	}
+
+	vector<short> anyWinningMoves(ToeBoard ttt, bool whoAmI)
+	// Returns an {x,y} winning move if it exists, otherwise returns {-1,-1}
+	{
+		// Tries every move
+		for (short a=0; a<3; ++a) {
+			for (short b=0; b<3; ++b) {
+				if ( tryTurn(ttt, a,b) == whoAmI ) {
+					vector<short> VICTORY = {a, b};
+					return(VICTORY);
+				}
+			}
+		}
+		// If we've tried everything and haven't returned a victory yet
+		vector<short> FAILURE = {3, 3};
+		return(FAILURE);
+	}
+/*
+	vector<vector<short>> goodIdeas(ToeBoard ttt, bool whoAmI)
+	// Returns a list of "good" moves
+	{
+//		vector<vector<short>> defensive_moves;
+		vector<vector<short>> remaining_moves;
+		short result;
+
+		// Tries every move
+		for (auto a=0; a<3; ++a) {
+			for (auto b=0; b<3; ++b) {
+				result = tryTurn(ttt, a,b);
+
+				// If the move means you win, IMMEDIATELY do so
+				if (result == whoAmI)
+				{
+					vector<vector<short>> WINNING_MOVE = {{a,b}};
+					return (WINNING_MOVE);
+				}
+				// If the move is at least POSSIBLE
+				else if (result != 7)
+				{ remaining_moves.push_back({a,b}); }
+			}
+		}
+		return remaining_moves;
+
+//		// If there aren't any winning moves, send off your defensive ones!
+//		if (defensive_moves.size() != 0)
+//		{ return defensive_moves; }
+//		// And if there aren't any defensive moves, send off literally anything else!
+//		else
+//		{ return remaining_moves; }
+	}
+*/
+
 	void takeTurn(ToeBoard &ttt)
 	// Brute-force tries to pick the next empty place in-sequence
 	{
-		if( ttt.gameOver() == -1)
+		bool whoAmI = ttt.whoseTurn();
+		if (ttt.gameOver() == -1)
 		{
+//			if (ttt.anyWinningMoves(ttt, != {3,3} )
+			
 			// Try the center and some corners first
-			if (ttt.isEmpty(1,1))		{ ttt.takeTurn(1,1,ttt.whoseTurn()); }
-			else if (ttt.isEmpty(0,0))	{ ttt.takeTurn(0,0,ttt.whoseTurn()); }
-			else if (ttt.isEmpty(2,0))	{ ttt.takeTurn(2,0,ttt.whoseTurn()); }
-			else if (ttt.isEmpty(0,2))	{ ttt.takeTurn(0,2,ttt.whoseTurn()); }
+			if (ttt.isEmpty(1,1))	{ ttt.takeTurn(1,1,whoAmI); }
+			else if (ttt.isEmpty(0,0))	{ ttt.takeTurn(0,0,whoAmI); }
+			else if (ttt.isEmpty(2,0))	{ ttt.takeTurn(2,0,whoAmI); }
+			else if (ttt.isEmpty(0,2))	{ ttt.takeTurn(0,2,whoAmI); }
 
 			// Then try the edges
-			else if (ttt.isEmpty(0,1))	{ ttt.takeTurn(0,1,ttt.whoseTurn()); }
-			else if (ttt.isEmpty(2,1))	{ ttt.takeTurn(2,1,ttt.whoseTurn()); }
-			else if (ttt.isEmpty(1,0))	{ ttt.takeTurn(1,0,ttt.whoseTurn()); }
-			else if (ttt.isEmpty(1,2))	{ ttt.takeTurn(1,2,ttt.whoseTurn()); }
+			else if (ttt.isEmpty(0,1))	{ ttt.takeTurn(0,1,whoAmI); }
+			else if (ttt.isEmpty(2,1))	{ ttt.takeTurn(2,1,whoAmI); }
+			else if (ttt.isEmpty(1,0))	{ ttt.takeTurn(1,0,whoAmI); }
+			else if (ttt.isEmpty(1,2))	{ ttt.takeTurn(1,2,whoAmI); }
 
 			// Final corner
-			else if (ttt.isEmpty(2,2))	{ ttt.takeTurn(2,2,ttt.whoseTurn()); }
+			else if (ttt.isEmpty(2,2))	{ ttt.takeTurn(2,2,whoAmI); }
 
 			// Uh
 			else
--- TicTac/Test_AI.cpp
+++ TicTac/Test_AI.cpp
@@ -74,11 +74,137 @@ void Test01()
 	cout << "After AI\n" << t9;
 }
 
+void Test02()
+// We test if the AI is able to correctly forsee the immediate outcome of a move
+{
+	int Tests = 0, Successes = 0;
+
+	AI ToeBot;
+	ToeBoard t0;
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,0,0) == -1) {Successes += 1;}
+	else { cout << "Failed Test02.001\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,0,1) == -1) {Successes += 1;}
+	else { cout << "Failed Test02.002\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,0,2) == -1) {Successes += 1;}
+	else { cout << "Failed Test02.003\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,1,0) == -1) {Successes += 1;}
+	else { cout << "Failed Test02.004\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,1,1) == -1) {Successes += 1;}
+	else { cout << "Failed Test02.005\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,2,1) == -1) {Successes += 1;}
+	else { cout << "Failed Test02.006\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,0,2) == -1) {Successes += 1;}
+	else { cout << "Failed Test02.007\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,1,2) == -1) {Successes += 1;}
+	else { cout << "Failed Test02.008\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,2,2) == -1) {Successes += 1;}
+	else { cout << "Failed Test02.009\n"; }
+
+	t0.takeTurn(0,0,0);
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,0,0) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.010\n"; }
+
+	t0.takeTurn(1,1,0);
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,0,0) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.011\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,1,1) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.012\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,2,2) == -1) {Successes += 1;}
+	else { cout << "Failed Test02.013\n"; }
+
+	t0.takeTurn(1,0,1);
+	t0.takeTurn(0,1,1);
+	t0.takeTurn(0,2,1);
+
+	// First attempt at identifying a potential victory
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,2,2) == 0) {Successes += 1;}
+	else { cout << "Failed Test02.014\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,1,0) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.015\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,0,1) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.016\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,0,2) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.017\n"; }
+
+	// Time to verify the game is over
+	t0.takeTurn(2,2,0);
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,0,0) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.021\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,0,1) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.022\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,0,2) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.023\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,1,0) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.024\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,1,1) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.025\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,2,1) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.026\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,0,2) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.027\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,1,2) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.028\n"; }
+
+	Tests += 1;
+	if (ToeBot.tryTurn(t0,2,2) == 7) {Successes += 1;}
+	else { cout << "Failed Test02.029\n"; }
+
+	cout << "Tests:\t\t" << Tests << "\nSuccesses:\t" << Successes << "\n";
+}
 
 int main()
 {
 	cout << "\n~~~~~\tTEST01()\t~~~~~\n";	Test01();
-//	cout << "\n~~~~~\tTEST02()\t~~~~~\n";	Test02();
+	cout << "\n~~~~~\tTEST02()\t~~~~~\n";	Test02();
 //	cout << "\n~~~~~\tTEST03()\t~~~~~\n";	Test03();
 //	cout << "\n~~~~~\tTEST04()\t~~~~~\n";	Test04();
 //	cout << "\n~~~~~\tTEST05()\t~~~~~\n";	Test05();
